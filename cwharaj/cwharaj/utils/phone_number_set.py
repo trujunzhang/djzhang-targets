1a282f77d1125fae17b55f05af89db49a1acbd71
trujunzhang <trujunzhang@gmail.com>
2016-06-09 17:26:52 +0800

    cwharaj.

--- cwharaj/cwharaj/utils/phone_number_set.py
+++ cwharaj/cwharaj/utils/phone_number_set.py
@@ -10,10 +10,12 @@ class PhoneNumberSet(object):
 
     def add_row(self, model_id, row):
         self.dict[model_id] = row
-        logging.debug("Get ajax url and added to dict for {}".format(model_id))
+        logging.debug("Added to dict for {}".format(model_id))
+        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
     def get_page_url_from_ajax_url(self, _ajax_url, _phone_number_base64):
         logging.debug("Get page url from ajax url:")
+        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
         model_id = CrawlUtils.get_model_id_from_phone_number_url(_ajax_url)
         logging.debug("  1. model_id: {}".format(model_id))
@@ -31,6 +33,7 @@ class PhoneNumberSet(object):
 
     def get_phone_number_base64(self, model_id):
         logging.debug("Get phone number base64 from dict:")
+        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
         row = self.dict[model_id]
         logging.debug("  1. model_id: {}".format(model_id))
@@ -51,6 +54,7 @@ class PhoneNumberSet(object):
 
     def remove_row(self, model_id):
         logging.debug("Remove row from dict:")
+        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
         logging.debug("  1. model_id: {}".format(model_id))
 
@@ -58,3 +62,4 @@ class PhoneNumberSet(object):
             logging.debug("  2. row exist in the dict.")
             del self.dict[model_id]
             logging.debug("  3. deleted the row sucessfully: {}".format(model_id))
+            logging.debug("  4. dict keys: {}".format(self.dict.keys()))
