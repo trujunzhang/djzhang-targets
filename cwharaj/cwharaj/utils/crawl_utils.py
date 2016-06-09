b5fdbc4c4b4860064ca52597dd69016ece2b348c
trujunzhang <trujunzhang@gmail.com>
2016-06-09 17:32:51 +0800

    cwharaj.

--- cwharaj/cwharaj/utils/crawl_utils.py
+++ cwharaj/cwharaj/utils/crawl_utils.py
@@ -1,5 +1,5 @@
 from hashlib import md5
-import urlparse
+import logging
 
 
 class CrawlUtils(object):
@@ -14,5 +14,22 @@ class CrawlUtils(object):
 
     @classmethod
     def get_model_id_from_phone_number_url(self, _ajax_url):
+        import urlparse
         model_id = urlparse.parse_qs(urlparse.urlparse(_ajax_url).query)['model_id'][0]
         return model_id
+
+    @classmethod
+    def get_model_id_from_page_url(self, _page_url):
+        logging.debug("Get model_id from page url:")
+
+        from urlparse import urlparse
+        array = urlparse(_page_url).path.split('/')
+
+        logging.debug("  1. array: {}".format(len(array)))
+        if len(array) >= 3:
+            model_id = array[3]
+            logging.debug("  2. model_id: {}".format(model_id))
+            return model_id
+
+        logging.debug("  3. split the page url failure: {}".format(_page_url))
+        return None
