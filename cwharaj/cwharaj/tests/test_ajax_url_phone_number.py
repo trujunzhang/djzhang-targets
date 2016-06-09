96fd6c7833b61e5af549f2b3c098397fb2e665ea
trujunzhang <trujunzhang@gmail.com>
2016-06-09 18:39:23 +0800

    cwharaj.

--- cwharaj/cwharaj/tests/test_ajax_url_phone_number.py
+++ cwharaj/cwharaj/tests/test_ajax_url_phone_number.py
@@ -8,7 +8,8 @@ class AjaxUrlPhoneNumberTest(unittest.TestCase):
         self._ajax_url = "https://sa.opensooq.com/ar/post/get-phone-number?model_id={}&model_type=post".format(
             self.model_id)
 
-        self._page_url = 'https://sa.opensooq.com/ar/search/39319909/wanghao'
+        # self._page_url = 'https://sa.opensooq.com/ar/search/39319909/wanghao'
+        self._page_url = 'https://sa.opensooq.com/ar/search/41957149/%D8%B9%D9%85%D8%A7%D8%B1%D8%A9-%D9%81%D9%8A-%D8%AD%D9%8A-%D8%A7%D9%84%D8%B1%D8%A7%D9%8A%D8%A9-%D9%84%D9%84%D8%AA%D9%88%D8%A7%D8%B5%D9%84-0541080350'
 
     def test_regex_get_model_id(self):
         from urlparse import urlparse
