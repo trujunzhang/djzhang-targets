beb2f65c4e760a1d28387c7dc1253afe83e61377
trujunzhang <trujunzhang@gmail.com>
2016-06-10 17:41:59 +0800

    cwharaj.

--- cwharaj/cwharaj/parser/harajsa_parser.py
+++ cwharaj/cwharaj/parser/harajsa_parser.py
@@ -60,13 +60,11 @@ class HarajSaParse(BaseParser):
         _subject = ""
         _contact = ""
         _number = self.get_value_from_response(hxs, '//*[@class="contact"]/strong/a/text()')
-
         _address = self.get_value_from_response(hxs,
-                                                '//*[@class="boxItem"]/table[3]/tr/td[1]/a/text()')
-
+                                                '//*[@class="boxItem"]/table[3]/tr/td[1]/a/text()') # ??
         _description = self.get_all_value_from_response(hxs, '//*[@itemprop="description"]/text()')
 
-        _section = self.get_value_from_response(hxs, '//*[@class="boxItem"]/table[2]/tr/td[1]/a/text()')
+        _section = self.get_value_from_response(hxs, '//*[@class="ad_low"]/div/a[@class="tag fromad"]/span/text()')
 
         # Replace "\n","\r"
         _description = _description.replace("\r", "").strip()
