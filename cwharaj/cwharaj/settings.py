9745b43ae86a1f4f02476cd9d372b30a88a95c77
trujunzhang <trujunzhang@gmail.com>
2016-06-11 23:14:54 +0800

    cwharaj.

--- cwharaj/cwharaj/settings.py
+++ cwharaj/cwharaj/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'cwharaj.spiders'
 #USER_AGENT = 'cwharaj (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=2
+CONCURRENT_REQUESTS=4
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
