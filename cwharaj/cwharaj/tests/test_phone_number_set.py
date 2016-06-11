a02a46b268596e17f9af64a658b72eeb3752ff5d
trujunzhang <trujunzhang@gmail.com>
2016-06-11 15:10:13 +0800

    cwharaj.

--- cwharaj/cwharaj/tests/test_phone_number_set.py
+++ cwharaj/cwharaj/tests/test_phone_number_set.py
@@ -5,13 +5,18 @@ from cwharaj.items import CacheItem
 from cwharaj.utils.crawl_utils import CrawlUtils
 from cwharaj.utils.phone_number_set import PhoneNumberSet
 
+
 class PhoneNumberSetTest(unittest.TestCase):
     def setUp(self):
         self.phoneNumber = PhoneNumberSet()
-        self._ajax_url = "https://sa.opensooq.com/ar/post/get-phone-number?model_id={}&model_type=post".format("123")
+        self._ajax_url = "https://sa.opensooq.com/ar/post/get-phone-number?model_id={}&model_type={}".format("", "")
         self.page_url = "https://sa.opensooq.com/ar/search/40616083/%D9%81%D9%8A%D9%84%D8%A7-%D8%A8%D8%A7%D9%84%D8%A7%D8%B3%D9%83%D8%A7%D9%86-%D8%A7%D9%84%D8%AC%D8%AF%D9%8A%D8%AF"
         row = CacheItem(
+            ID="123",
             url=self.page_url,
             guid=CrawlUtils.get_guid(self.page_url)
         )
-        self.phoneNumber.add_row(dict(row), self._ajax_url)
+        self.phoneNumber.add_row(row['ID'], row)
+
+    def test_get_id_from_phone_number_url(self):
+        CrawlUtils.get_id_from_phone_number_url(self._ajax_url)
