e64a695d17dea9c6b2780de7622eb792f9dbed19
trujunzhang <trujunzhang@gmail.com>
2016-06-12 23:05:45 +0800

    cwharaj.

---
+++ cwharaj/cwharaj/tests/test_arabic_reshaper.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+import unittest
+
+from bidi.algorithm import get_display
+
+from cwharaj.utils import arabic_reshaper
+
+
+class ArabicReshaperTest(unittest.TestCase):
+    def setUp(self):
+        self.aribic = "https://sa.opensooq.com/ar/search/30002057/استراحة-سديم-للايجار-اليومي-والشهري-والسنوي-حي-الأمانة-شمال-الرياض"
+
+    def test_aribic_ascii(self):
+        _split = self.aribic.split('/')[6]
+        reshaped_text = arabic_reshaper.reshape(_split.encode('utf-8'))
+        bidi_text = get_display(reshaped_text)
+        # pass_arabic_text_to_render(bidi_text)
