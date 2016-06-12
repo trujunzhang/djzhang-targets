013e5240d6ab3f01b8e71e252da8bb6c34e7c000
trujunzhang <trujunzhang@gmail.com>
2016-06-12 11:02:08 +0800

    cwharaj.

--- cwharaj/cwharaj/database/base/base_db.py
+++ cwharaj/cwharaj/database/base/base_db.py
@@ -14,5 +14,26 @@ class BaseDatabase(object):
     def process_item(self, url, item=None, index=0, id=-1):
         pass
 
+    def insert_for_cache(self, item):
+        pass
+
+    def insert_for_history(self, item):
+        pass
+
+    def insert_for_item(self, item):
+        pass
+
+    def update_for_history(self, item):
+        pass
+
+    def get_count(self, dict):
+        pass
+
+    def delete_row(self, dict):
+        pass
+
+    def find_for_cache(self):
+        pass
+
     def check_exist_by_id(self, _id):
         return False
