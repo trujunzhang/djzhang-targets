d9ccf9eca0e698751805feb041570f5d6b6cc04c
trujunzhang <trujunzhang@gmail.com>
2016-06-12 18:36:43 +0800

    cwharaj.

--- cwharaj/db/seed.sql
+++ cwharaj/db/seed.sql
@@ -1,6 +1,7 @@
-CREATE TABLE hirah_cache (
+CREATE TABLE hiraj_cache (
   guid       CHAR(20)  NOT NULL,
   ID         INT       NOT NULL,
   url        CHAR(200) NOT NULL,
+  url_from   CHAR(50) NOT NULL,
   created_at CHAR(20)  NOT NULL
 )
