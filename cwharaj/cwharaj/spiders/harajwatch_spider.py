47493030de40347729033235d709fbadbe9df9d5
trujunzhang <trujunzhang@gmail.com>
2016-06-13 11:52:28 +0800

    cwharaj.

--- cwharaj/cwharaj/spiders/harajwatch_spider.py
+++ cwharaj/cwharaj/spiders/harajwatch_spider.py
@@ -58,8 +58,8 @@ class HarajsSpiderWatch(scrapy.Spider):
     def parse(self, response):
         # Here, 3 website need to fetch again and again(Through 24X7, full-time)
         # yield scrapy.Request(self.url_from_opensooq, callback=self.parse_pagination_from_opensooq, dont_filter=True)
-        # yield scrapy.Request(self.url_from_mstaml, callback=self.parse_pagination_from_mstaml, dont_filter=True)
-        yield scrapy.Request(self.url_from_harajsa, callback=self.parse_pagination_from_harajsa, dont_filter=True)
+        yield scrapy.Request(self.url_from_mstaml, callback=self.parse_pagination_from_mstaml, dont_filter=True)
+        # yield scrapy.Request(self.url_from_harajsa, callback=self.parse_pagination_from_harajsa, dont_filter=True)
 
     def parse_pagination_from_opensooq(self, response):
         # Step 1: parse all list items to the cache database.
