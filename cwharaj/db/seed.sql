22882fbf2ae1794bc0f5014b217b1eea4016fe58
trujunzhang <trujunzhang@gmail.com>
2016-06-19 20:34:02 +0800

    charset="utf8mb4",

--- cwharaj/db/seed.sql
+++ cwharaj/db/seed.sql
@@ -16,26 +16,39 @@ CREATE TABLE ads_histories (
 )
   DEFAULT CHARSET = utf8mb4;
 
+--
+-- Table structure for table `ads`
+--
 
-CREATE TABLE ads (
-  guid        CHAR(32) PRIMARY KEY,
-  url         TEXT,
-  created_at  DATETIME,
-  updated_at  DATETIME,
-
-  ID          TEXT,
-  city        TEXT,
-  time        TEXT,
-  title       TEXT,
-  pictures    TEXT,
-  subject     TEXT,
-  contact     TEXT,
-  number      TEXT,
-  url_from    TEXT,
-
-  address     TEXT,
-  memberName  TEXT,
-  description TEXT,
-  section     TEXT
+CREATE TABLE IF NOT EXISTS `ads` (
+  `id`                   INT(11)      NOT NULL AUTO_INCREMENT,
+  `ads_title`            VARCHAR(100) NOT NULL,
+  `ads_city`             VARCHAR(100) NOT NULL,
+  `ads_tags_R`           VARCHAR(100) NOT NULL,
+  `ads_tags_F`           VARCHAR(100) NOT NULL,
+  `ads_tags_FF`          VARCHAR(100) NOT NULL,
+  `ads_contact`          VARCHAR(100) NOT NULL,
+  `ads_body`             LONGTEXT     NOT NULL,
+  `image_link`           LONGTEXT     NOT NULL,
+  `type_ads_other_final` VARCHAR(100) NOT NULL,
+  `un_model`             VARCHAR(100) NOT NULL,
+  `status`               INT(100)     NOT NULL,
+  `fixing`               INT(100)     NOT NULL,
+  `Time_added`           INT(11)      NOT NULL,
+  `His_announcement`     INT(11)      NOT NULL,
+  `type_ads_or`          INT(1)       NOT NULL,
+  `close_ads`            INT(11)      NOT NULL,
+  `Last_updated_Ad`      INT(11)      NOT NULL,
+  `closecomment`         INT(11)      NOT NULL,
+  `fixed_home`           INT(11)      NOT NULL,
+  `fixed_tub`            INT(11)      NOT NULL,
+  `fixed_sec`            INT(11)      NOT NULL,
+  `fixed_sec2`           INT(11)      NOT NULL,
+  `fixed_sec3`           INT(11)      NOT NULL,
+  `timer_mazad`          INT(11)      NOT NULL,
+  PRIMARY KEY (`id`)
 )
-  DEFAULT CHARSET = utf8mb4;
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8
+  AUTO_INCREMENT = 24;
+
