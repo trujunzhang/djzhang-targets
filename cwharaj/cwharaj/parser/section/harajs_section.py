b64493bf12e4b0bf17c222488e771e88c7c754db
trujunzhang <trujunzhang@gmail.com>
2016-06-20 15:53:31 +0800

    charset="utf8mb4",

---
+++ cwharaj/cwharaj/parser/section/harajs_section.py
@@ -0,0 +1,13 @@
+class HarajsSection(object):
+    """
+    types:
+    t1:
+       ES-ES2016-(xxx)
+       F-(years)FF-R
+    t2:
+       ES-ES2001-FS-FS2001
+       F-(years)FF-R
+    """
+
+    def __init__(self):
+        super(HarajsSection, self).__init__()
