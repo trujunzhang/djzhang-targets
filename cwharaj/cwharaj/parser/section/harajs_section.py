3da57679b92558d3226dd5dd4ab87768688287b2
trujunzhang <trujunzhang@gmail.com>
2016-06-20 21:31:28 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/parser/section/harajs_section.py
+++ cwharaj/cwharaj/parser/section/harajs_section.py
@@ -17,17 +17,18 @@ class HarajsSection(object):
 
     def get_section_item(self, _sections, item_db):
         _section_item = SectionItem()
+
         self.get_tagF(_sections, item_db)
+
         return _section_item
 
     def get_tagF(self, _sections, item_db):
         _count = len(_sections)
-        for x in xrange(0, _count):
-            _index = _count - 1 - x
-            _section = _sections[_index]
+        for x in xrange(_count - 1, -1, -1):
+            _section = _sections[x]
             _split = _section.split(' ')
             _len = len(_split)
-            if (_len == 2) and (_index != 0):
+            if (_len == 2) and (x != 0):
                 self.parse_tagFF(_sections, _split, x - 1, item_db)
 
     def parse_tagFF(self, _sections, _split, pre_index, item_db):
