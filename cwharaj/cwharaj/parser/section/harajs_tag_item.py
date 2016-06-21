50a4bdd46c4abcf927baff208b8785b8c599043c
trujunzhang <trujunzhang@gmail.com>
2016-06-21 10:50:27 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/parser/section/harajs_tag_item.py
+++ cwharaj/cwharaj/parser/section/harajs_tag_item.py
@@ -8,7 +8,6 @@ class TagItem(object):
     tag_F = ""
     tag_FF = ""
     tag_FF_index = -1
-    tag_R_index = -1
 
     def __init__(self, sections, item_db):
         super(TagItem, self).__init__()
@@ -21,23 +20,24 @@ class TagItem(object):
         self.tag_FF_index = x
 
     def get_index_tag_r(self):
+        _tag_r_index = -1
         if (self.tag_FF != "") and (self.sections_count == 3):
             # sections: ["2016 xxx","xxx","YYY"]
             if self.tag_FF_index == 2:
-                self.tag_R_index = 0
+                _tag_r_index = 0
             # sections: ["YYY","2016 xxx","xxx"]
             elif self.tag_FF_index == 1:
-                self.tag_R_index = 2
+                _tag_r_index = 2
 
         if (self.tag_FF != "") and (self.sections_count == 2):
             logging.debug("no tag_r in the sections, count: {}".format(self.sections_count))
-            self.tag_R_index = -1
+            _tag_r_index = -1
 
         if (self.tag_FF == "") and (self.sections_count >= 1):
             logging.debug("special sections, count: {}".format(self.sections_count))
-            self.tag_R_index = -1
+            _tag_r_index = -1
 
-
+        return _tag_r_index
 
     def parse_common_tag_item(self):
         for x in xrange(self.sections_count - 1, -1, -1):
