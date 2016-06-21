ef8ef3bcd4a3310a9f9e7450ec494173b17f5e33
trujunzhang <trujunzhang@gmail.com>
2016-06-21 10:40:33 +0800

    charset="utf8mb4",

---
+++ cwharaj/cwharaj/tests/test_harajsa_section.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+import unittest
+
+from cwharaj import settings
+from cwharaj.database_factory import DatabaseFactory, CollectionTypes
+from cwharaj.parser.section.harajs_section import HarajsSection
+
+
+class HarajsSecionTest(unittest.TestCase):
+    def setUp(self):
+        database_factory = DatabaseFactory(settings.SQL_HOST, settings.SQL_PORT,
+                                           settings.SQL_USER, settings.SQL_PASSWD,
+                                           settings.SQL_DB, settings.SQL_COLLECTION_NAME)
+
+        self._item_db = database_factory.get_database(CollectionTypes.item)
+        _sections = ["كامري 2016", "كامري", "تويوتا"]
+        self.section_mgr = HarajsSection(_sections, self._item_db)
+
+    def test_parse_section(self):
+        _section_item = self.section_mgr.get_section_item()
