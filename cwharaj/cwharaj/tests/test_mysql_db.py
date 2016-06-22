c820fe9bff00fe56154e2a95dc31be559a04bddd
trujunzhang <trujunzhang@gmail.com>
2016-06-22 11:08:44 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/tests/test_mysql_db.py
+++ cwharaj/cwharaj/tests/test_mysql_db.py
@@ -107,7 +107,7 @@ class MysqlDBTest(unittest.TestCase):
 
     def test_insert_new_city(self):
         _cities_row = {
-            "id": 24,
+            "id": 26,
             "text": "دبي",
             "sql": "INSERT INTO cities(text) VALUES ('دبي')"
         }
