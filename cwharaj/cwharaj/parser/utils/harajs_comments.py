0af7aae810c8df81d967242f34a4b71f65f2ce00
trujunzhang <trujunzhang@gmail.com>
2016-06-22 18:17:26 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/parser/utils/harajs_comments.py
+++ cwharaj/cwharaj/parser/utils/harajs_comments.py
@@ -9,13 +9,12 @@ class HarajsComments(object):
         self.item_db = item_db
         self.id_ads = id_ads
 
-
     def save_for_harajs(self, hxs):
         _comments_selector = '//*[@class="comment comment_div"]'
         _comments_div = hxs.xpath(_comments_selector)
         for _comment_div in _comments_div:
-            _memberName = self.baseParser.get_value_from_response(_comments_selector + '/div/div/a/text()')
-            _content = self.baseParser.get_value_from_response(_comments_selector + '/div[@class="comment_body]')
+            _memberName = self.baseParser.get_value_from_response(hxs, _comments_selector + '/div/div/a/text()')
+            _content = self.baseParser.get_value_from_response(hxs, _comments_selector + '/div[@class="comment_body]')
 
             id_His_response = self.item_db.save_member(Member.get_default(_memberName))
             comment = Comment.get_default(self.id_ads, id_His_response, _content)
