9a3f9cebb618723dd2128f8f9501159fd993aef2
trujunzhang <trujunzhang@gmail.com>
2016-06-22 09:28:12 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/tests/test_mysql_db.py
+++ cwharaj/cwharaj/tests/test_mysql_db.py
@@ -6,7 +6,7 @@ import time
 
 from cwharaj.database_factory import DatabaseFactory, CollectionTypes
 from cwharaj.utils.crawl_utils import CrawlUtils
-from cwharaj.items import CacheItem, HistoryItem, Ad, WebsiteTypes
+from cwharaj.items import CacheItem, HistoryItem, Ad, WebsiteTypes, City
 from datetime import datetime
 from cwharaj import settings
 
@@ -24,74 +24,80 @@ class MysqlDBTest(unittest.TestCase):
         self._cache_url = "https://sa.opensooq.com/ar/search/30002057/استراحة-سديم-للايجار-اليومي-والشهري-والسنوي-حي-الأمانة-شمال-الرياض"
         self._cache_from = WebsiteTypes.opensooq.value
 
-    def test_insert_cache_row(self):
-        _guid = "1234321"
-        _id = CrawlUtils.url_parse_id_from_page_url(self._cache_url, 3)
+    # def test_insert_cache_row(self):
+    #     _guid = "1234321"
+    #     _id = CrawlUtils.url_parse_id_from_page_url(self._cache_url, 3)
+    #
+    #     self._cache_db.open_spider()
+    #     item = CacheItem(
+    #         url=self._cache_url,
+    #         guid=_guid,
+    #         created_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
+    #         ID=_id,
+    #         url_from=self._cache_from
+    #     )
+    #     self._cache_db.insert_for_cache(item)
 
-        self._cache_db.open_spider()
-        item = CacheItem(
-            url=self._cache_url,
-            guid=_guid,
-            created_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
-            ID=_id,
-            url_from=self._cache_from
-        )
-        self._cache_db.insert_for_cache(item)
-
-        # def test_oldest_cache(self):
-        #     self.mysql_database.open_spider()
-        #
-        #     # row = self.mysql_database.find_oldest_for_cache()
-        #
-        #     self.mysql_database.delete_row(self._cache_url, self._cache_from)
+    # def test_oldest_cache(self):
+    #     self._cache_db.open_spider()
+    #
+    #     # row = self._cache_db.find_oldest_for_cache()
+    #
+    #     self._cache_db.delete_row(self._cache_url, self._cache_from)
 
 
-        # def test_insert_history_row(self):
-        #     _url = "https://sa.opensooq.com/ar/search/30002057/استراحة-سديم-للايجار-اليومي-والشهري-والسنوي-حي-الأمانة-شمال-الرياض"
-        #     _guid = "1234321"
-        #     # _id = CrawlUtils.url_parse_id_from_page_url(_url, 3)
-        #     _id = "123"
-        #
-        #     self.mysql_database.open_spider()
-        #     item = HistoryItem(
-        #         url=_url,
-        #         guid=_guid,
-        #         created_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
-        #         ID=_id
-        #     )
-        #     self.mysql_database.update_for_history(_id, item)
+    # def test_insert_history_row(self):
+    #     _url = "https://sa.opensooq.com/ar/search/30002057/استراحة-سديم-للايجار-اليومي-والشهري-والسنوي-حي-الأمانة-شمال-الرياض"
+    #     _guid = "1234321"
+    #     # _id = CrawlUtils.url_parse_id_from_page_url(_url, 3)
+    #     _id = "123"
+    #
+    #     self._history_db.open_spider()
+    #     item = HistoryItem(
+    #         url=_url,
+    #         guid=_guid,
+    #         created_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
+    #         ID=_id
+    #     )
+    #     self._history_db.update_for_history(_id, item)
 
-        # def test_insert_item_row(self):
-        #     _url = "https://sa.opensooq.com/ar/search/30002057/استراحة-سديم-للايجار-اليومي-والشهري-والسنوي-حي-الأمانة-شمال-الرياض"
-        #     _guid = "1234321"
-        #     # _id = CrawlUtils.url_parse_id_from_page_url(_url, 3)
-        #     _id = "123"
-        #
-        #     # _section = ['section']
-        #     _section = []
-        #
-        #     self.mysql_database.open_spider()
-        #     item = Ad(
-        #         url=_url,
-        #         guid=_guid,
-        #         created_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
-        #         updated_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
-        #
-        #         ID=_id,
-        #         city='city',
-        #         time='time',
-        #         title='title',
-        #         pictures=['pic1', 'pic2'],
-        #         subject='subject',
-        #         contact='contact',
-        #         number='number',
-        #
-        #         # cache form where, such as opensooq,mstaml.(WebsiteTypes variable)
-        #         url_from='opensooq',
-        #
-        #         address='address',
-        #         memberName='member name',
-        #         description='description',
-        #         section=_section,
-        #     )
-        #     self.mysql_database.insert_for_item(item)
+    # def test_insert_item_row(self):
+    #     _url = "https://sa.opensooq.com/ar/search/30002057/استراحة-سديم-للايجار-اليومي-والشهري-والسنوي-حي-الأمانة-شمال-الرياض"
+    #     _guid = "1234321"
+    #     # _id = CrawlUtils.url_parse_id_from_page_url(_url, 3)
+    #     _id = "123"
+    #
+    #     # _section = ['section']
+    #     _section = []
+    #
+    #     self._item_db.open_spider()
+    #     item = Ad(
+    #         url=_url,
+    #         guid=_guid,
+    #         created_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
+    #         updated_at=datetime.utcnow().replace(microsecond=0).isoformat(' '),
+    #
+    #         ID=_id,
+    #         city='city',
+    #         time='time',
+    #         title='title',
+    #         pictures=['pic1', 'pic2'],
+    #         subject='subject',
+    #         contact='contact',
+    #         number='number',
+    #
+    #         # cache form where, such as opensooq,mstaml.(WebsiteTypes variable)
+    #         url_from='opensooq',
+    #
+    #         address='address',
+    #         memberName='member name',
+    #         description='description',
+    #         section=_section,
+    #     )
+    #     self._item_db.insert_for_item(item)
+
+    def test_insert_exist_city(self):
+        _ads_city = "الجوف"
+        _city_id = self._item_db.save_city(City.get_default(_ads_city))
+        expect = 12
+        self.assertEqual(expect, _city_id)
