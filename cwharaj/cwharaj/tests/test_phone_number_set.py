d47867e6fdaaaa11516e158229e2819b3f8479c5
trujunzhang <trujunzhang@gmail.com>
2016-06-28 23:06:28 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/tests/test_phone_number_set.py
+++ cwharaj/cwharaj/tests/test_phone_number_set.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import unittest
 from datetime import datetime
 
@@ -10,19 +11,11 @@ class PhoneNumberSetTest(unittest.TestCase):
     def setUp(self):
         self.phoneNumber = PhoneNumberSet()
         self._ajax_url = "https://sa.opensooq.com/ar/post/get-phone-number?model_id={}&model_type={}".format("", "")
-        self.url = "https://sa.opensooq.com/ar/search/40616083/%D9%81%D9%8A%D9%84%D8%A7-%D8%A8%D8%A7%D9%84%D8%A7%D8%B3%D9%83%D8%A7%D9%86-%D8%A7%D9%84%D8%AC%D8%AF%D9%8A%D8%AF"
-        row = CacheItem(
-            id="123",
-            url=self.url,
-            guid=CrawlUtils.get_guid(self.url)
-        )
-        self.phoneNumber.add_row(row['id'])
+        self.url = 'https://sa.opensooq.com/ar/search/40616083/فيلا-بالاسكان-الجديد'
+        self.model_id = '40616083'
 
-    # def test_get_id_from_phone_number_url(self):
-    #     CrawlUtils.get_id_from_phone_number_url(self._ajax_url)
-
-
-    def test_mysql_microsecond(self):
-        dt = datetime.now()
-        value = dt.microsecond
-        x = 0
+    def test_get_phone_number_item(self):
+        self.assertEqual(len(self.phoneNumber.dict.keys()), 0)
+        phone_number_item = self.phoneNumber.get_phone_number_item(self.url, self.model_id)
+        self.assertEqual(phone_number_item.model_id, self.model_id)
+        self.assertEqual(len(self.phoneNumber.dict.keys()), 1)
