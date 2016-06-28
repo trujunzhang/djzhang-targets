47b10348696faf00d5ad027a9a37dff1b2e5917e
trujunzhang <trujunzhang@gmail.com>
2016-06-28 22:58:26 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/tests/test_ajax_url_phone_number.py
+++ cwharaj/cwharaj/tests/test_ajax_url_phone_number.py
@@ -8,8 +8,15 @@ class AjaxUrlPhoneNumberTest(unittest.TestCase):
 
         # self._page_url = 'https://sa.opensooq.com/ar/search/39319909/wanghao'
         self._page_url = 'https://sa.opensooq.com/ar/search/41957149/%D8%B9%D9%85%D8%A7%D8%B1%D8%A9-%D9%81%D9%8A-%D8%AD%D9%8A-%D8%A7%D9%84%D8%B1%D8%A7%D9%8A%D8%A9-%D9%84%D9%84%D8%AA%D9%88%D8%A7%D8%B5%D9%84-0541080350'
+        self.expect = '41957149'
 
     def test_regex_get_id(self):
         from urlparse import urlparse
+
         _id = urlparse(self._page_url).path.split('/')[3]
-        _len = len(_id)
+        self.assertEqual(_id, self.expect)
+
+    def test_parse_id_from_page_url(self):
+        from cwharaj.utils.crawl_utils import CrawlUtils
+        _ID = CrawlUtils.url_parse_id_from_page_url(self._page_url, 3)
+        self.assertEqual(_ID, self.expect)
