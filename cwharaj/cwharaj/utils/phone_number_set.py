864a39ca58f9e413d628a15ffc0eea22eadf667f
trujunzhang <trujunzhang@gmail.com>
2016-06-28 23:40:03 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/utils/phone_number_set.py
+++ cwharaj/cwharaj/utils/phone_number_set.py
@@ -11,7 +11,7 @@ class PhoneNumberItem(object):
     phone_data_id = ''
     phone_data_type = ''
     _His_announcement_id = ''
-    id_ads = '-1'
+    id_ads = '-1'  # -1 means that no row on the database.
 
     def __init__(self, url, model_id):
         self.url = url
