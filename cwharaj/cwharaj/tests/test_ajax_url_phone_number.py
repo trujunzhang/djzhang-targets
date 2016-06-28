219e9e996e7f91165e7af2f12c99efbb73f5aaf3
trujunzhang <trujunzhang@gmail.com>
2016-06-28 22:59:20 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/tests/test_ajax_url_phone_number.py
+++ cwharaj/cwharaj/tests/test_ajax_url_phone_number.py
@@ -6,17 +6,16 @@ class AjaxUrlPhoneNumberTest(unittest.TestCase):
         self._id = "123"
         self._ajax_url = "https://sa.opensooq.com/ar/post/get-phone-number?model_id={}&model_type=post".format(self._id)
 
-        # self._page_url = 'https://sa.opensooq.com/ar/search/39319909/wanghao'
-        self._page_url = 'https://sa.opensooq.com/ar/search/41957149/%D8%B9%D9%85%D8%A7%D8%B1%D8%A9-%D9%81%D9%8A-%D8%AD%D9%8A-%D8%A7%D9%84%D8%B1%D8%A7%D9%8A%D8%A9-%D9%84%D9%84%D8%AA%D9%88%D8%A7%D8%B5%D9%84-0541080350'
-        self.expect = '41957149'
+        self._page_url_opensooq = 'https://sa.opensooq.com/ar/search/41957149/عمارة-في-حي-الراية-للتواصل-0541080350'
+        self.expect_opensooq = '41957149'
 
     def test_regex_get_id(self):
         from urlparse import urlparse
 
-        _id = urlparse(self._page_url).path.split('/')[3]
-        self.assertEqual(_id, self.expect)
+        _id = urlparse(self._page_url_opensooq).path.split('/')[3]
+        self.assertEqual(_id, self.expect_opensooq)
 
     def test_parse_id_from_page_url(self):
         from cwharaj.utils.crawl_utils import CrawlUtils
-        _ID = CrawlUtils.url_parse_id_from_page_url(self._page_url, 3)
-        self.assertEqual(_ID, self.expect)
+        _ID = CrawlUtils.url_parse_id_from_page_url(self._page_url_opensooq, 3)
+        self.assertEqual(_ID, self.expect_opensooq)
