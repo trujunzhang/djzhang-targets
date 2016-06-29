01fae11674854291693e0dff5ca711a2c6d14646
trujunzhang <trujunzhang@gmail.com>
2016-06-29 23:31:12 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/tests/test_time_ago.py
+++ cwharaj/cwharaj/tests/test_time_ago.py
@@ -30,18 +30,19 @@ class TimeAgoTest(unittest.TestCase):
         # expect = int(time.time())
         # self.assertEqual(int_time, expect)
 
-        time_ago = ' قبل ساعه و 21 دقيقه في'  # Before an hour and 21 minutes in
+        # time_ago = ' قبل ساعه و 21 دقيقه في'  # Before an hour and 21 minutes in
+        # int_time = self.time_util.get_time_for_harajs(time_ago)
+        # expect = 1467214211
+        # self.assertEqual(int_time, expect)
+
+        time_ago = ' قبل 6 يوم و 2 ساعه في'  # Before 6 days, 2 hours
         int_time = self.time_util.get_time_for_harajs(time_ago)
-        expect = int(time.time())
+        expect = 1467214211
         self.assertEqual(int_time, expect)
 
-        # time_ago = ' قبل 6 يوم و 2 ساعه في'  # Before 6 days, 2 hours
         # time_ago = ' قبل 3 شهر و 2 أسبوع في'  # Before 3 months and 2 weeks in
         # time_ago = 'قبل 5 سنه و 10 شهر في'  # Before five years and 10 months in
-        #
-        # int_time = self.time_util.get_time_for_harajs(time_ago)
-        # expect = 1467043200.0
-        # self.assertEqual(int_time, expect)
+
 
         # def test_get_time_for_mstaml(self):
         #     time_ago = '2016.06.28'
