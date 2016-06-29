9f4c7f120938db1cd283f74dd0f7b0f6a908c1c8
trujunzhang <trujunzhang@gmail.com>
2016-06-29 16:28:49 +0800

    charset="utf8mb4",

--- cwharaj/cwharaj/parser/utils/timer_util.py
+++ cwharaj/cwharaj/parser/utils/timer_util.py
@@ -1,3 +1,20 @@
+# coding=utf-8
 class TimerUtil(object):
+    lang = {
+        "second": "ثانية",
+        "minute": "دقيقة",
+        "hour": "ساعة",
+        "month": "شهر",
+        "year": "عام",
+        "seconds": "ثواني",
+        "minutes": "دقائق",
+        "hours": "ساعات",
+        "months": "شهور",
+        "years": "سنوات",
+        "ago": "قبل",
+        "day": "يوم",
+        "days": "ايام",
+    }
+
     def __init__(self):
         super(TimerUtil, self).__init__()
