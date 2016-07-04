b108b96465c74dbb4089dc6b2ef2aee665cc7c08
trujunzhang <trujunzhang@gmail.com>
2016-07-04 14:22:29 +0800

    cwharaj.

--- cwharaj/cwharaj/tests/test_mysql_db.py
+++ cwharaj/cwharaj/tests/test_mysql_db.py
@@ -49,10 +49,12 @@ class MysqlDBTest(unittest.TestCase):
     #     row = self._cache_db.get_oldest_row(self._page_url, self._page_from)
 
     def test_insert_history_row(self):
-        ads_id = "123"
-
-        item = HistoryItem.get_default(self._page_url, ads_id, self._page_from)
+        item = HistoryItem.get_default(self._page_url, self._ads_id, self._page_from)
         self._history_db.save_history(item)
+
+    def test_exist_history_row(self):
+        item = HistoryItem.get_default(self._page_url, self._ads_id, self._page_from)
+        self._history_db.check_history_exist(item['model_id'])
 
     # def test_get_year_id(self):
     #     _years_row = {
