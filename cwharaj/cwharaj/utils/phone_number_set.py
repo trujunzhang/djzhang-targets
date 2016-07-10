6424515f71e4d1a4775d0331d4470d0f93625581
trujunzhang <trujunzhang@gmail.com>
2016-07-10 14:39:23 +0800

    cwharaj.

--- cwharaj/cwharaj/utils/phone_number_set.py
+++ cwharaj/cwharaj/utils/phone_number_set.py
@@ -34,12 +34,12 @@ class PhoneNumberSet(object):
 
     def add_row(self, url, model_id):
         self.dict[model_id] = PhoneNumberItem(url, model_id)
-        logging.debug("Added to dict for {}".format(model_id))
-        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
+        # logging.debug("Added to dict for {}".format(model_id))
+        # logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
     def get_phone_number_item(self, url, _model_id):
         logging.debug("Get phone number item:")
-        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
+        # logging.debug("  *. dict keys: {}".format(self.dict.keys()))
         if _model_id not in self.dict.keys():
             self.add_row(url, _model_id)
 
@@ -52,11 +52,11 @@ class PhoneNumberSet(object):
         return None
 
     def get_item_from_ajax_url(self, _ajax_url):
-        logging.debug("Get item from ajax url and remove dict:")
-        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
+        # logging.debug("Get item from ajax url and remove dict:")
+        # logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
         _phone_id = CrawlUtils.get_id_from_phone_number_url(_ajax_url)
-        logging.debug("  1. parse phone_id from the ajax url: {}".format(_phone_id))
+        # logging.debug("  1. parse phone_id from the ajax url: {}".format(_phone_id))
 
         if _phone_id:
             for key, value in self.dict.iteritems():
@@ -68,13 +68,12 @@ class PhoneNumberSet(object):
         logging.debug("  3. not found item from ajax url: {}".format(_ajax_url))
 
     def remove_row(self, model_id):
-        logging.debug("Remove row from dict:")
-        logging.debug("  *. dict keys: {}".format(self.dict.keys()))
+        # logging.debug("Remove row from dict:")
+        # logging.debug("  *. dict keys: {}".format(self.dict.keys()))
 
-        logging.debug("  1. id: {}".format(model_id))
+        # logging.debug("  1. id: {}".format(model_id))
 
         if model_id in self.dict:
-            logging.debug("  2. row exist in the dict")
+            # logging.debug("  2. row exist in the dict")
             del self.dict[model_id]
-            logging.debug("  3. deleted the dict row {}, successfully".format(model_id))
-            logging.debug("  4. after, dict keys: {}".format(self.dict.keys()))
+            logging.debug("  3. deleted the dict row {}, dict keys: {} successfully".format(model_id, self.dict.keys()))
