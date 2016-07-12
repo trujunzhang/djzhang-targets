3f5016bcfa223cff2ec3ddfeb400ed48a4d75bc2
trujunzhang <trujunzhang@gmail.com>
2016-07-12 18:28:48 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/settings.py
+++ cwpoliticl/cwpoliticl/settings.py
@@ -14,50 +14,49 @@ BOT_NAME = 'cwpoliticl'
 SPIDER_MODULES = ['cwpoliticl.spiders']
 NEWSPIDER_MODULE = 'cwpoliticl.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'cwpoliticl (+http://www.yourdomain.com)'
+# USER_AGENT = 'cwpoliticl (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=4
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY=2
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN=8
 # CONCURRENT_REQUESTS_PER_IP=8
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED=False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED=False
+# TELNETCONSOLE_ENABLED=False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'cwpoliticl.middlewares.MyCustomSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'cwpoliticl.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
@@ -68,21 +67,21 @@ COOKIES_ENABLED=False
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 # NOTE: AutoThrottle will honour the standard settings for concurrency and delay
-AUTOTHROTTLE_ENABLED=True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY=5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY=60
+AUTOTHROTTLE_MAX_DELAY = 60
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG=True
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED=True
-#HTTPCACHE_EXPIRATION_SECS=0
-#HTTPCACHE_DIR='httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES=[]
-#HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED=True
+# HTTPCACHE_EXPIRATION_SECS=0
+# HTTPCACHE_DIR='httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES=[]
+# HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # Retry many times since proxies often fail
 RETRY_TIMES = 10
@@ -103,13 +102,6 @@ DOWNLOADER_MIDDLEWARES = {
 # ...
 PROXY_LIST = '/var/scrapy/proxy/list.txt'
 
-
-MONGODB_SERVER = "localhost"
-# MONGODB_SERVER = "104.236.77.182"
-MONGODB_PORT = 27017
-MONGODB_DB = "vps_scrapy_rails"
-MONGODB_COLLECTION = "politicls"
-
 # SQL DATABASE SETTING
 SQL_HOST = 'localhost'
 # SQL_HOST = '104.236.77.182'
@@ -121,3 +113,10 @@ SQL_PASSWD = 'politicl720'
 SQL_DB = 'politicl'
 # table_name
 SQL_COLLECTION_NAME = 'politicl'
+
+# python-wordpress-xmlrpc
+WD_HOST = 'http://localhost:8888/wordpress'
+WD_HOST = 'http://192.168.1.106:8888'
+WD_HOST = 'http://104.236.77.182:8888'
+WD_USER = 'trujunzhang'
+WD_PASSWD = 'wanghao720'
