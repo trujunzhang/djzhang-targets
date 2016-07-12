9c6a6907d2c37c954efd8f9b5051651f8ce86d06
trujunzhang <trujunzhang@gmail.com>
2016-07-12 18:37:24 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/tests/test_wd_xml_rpc.py
+++ cwpoliticl/cwpoliticl/tests/test_wd_xml_rpc.py
@@ -11,22 +11,23 @@ from wordpress_xmlrpc.methods.users import GetUserInfo
 
 class WDXmlRpcTest(unittest.TestCase):
     def setUp(self):
-        self.opensooq_phone_id = 24
-
-    def test_post_page(self):
         url = "{}/xmlrpc.php".format(settings.WD_HOST)
-        wp = Client(url, settings.WD_USER, settings.WD_PASSWD)
-        wp.call(GetPosts())
+        self.wp = Client(url, settings.WD_USER, settings.WD_PASSWD)
+
+    def test_get_posts(self):
+        wp_call = self.wp.call(GetPosts())
         # [ < WordPressPost: hello - world(id=1) >]
 
-        wp.call(GetUserInfo())
+    def test_get_user_info(self):
+        wp_call = self.wp.call(GetUserInfo())
         # < WordPressUser: max >
 
-        post = WordPressPost()
-        post.title = 'My new title'
-        post.content = 'This is the body of my new post.'
-        post.terms_names = {
-            'post_tag': ['test', 'firstpost'],
-            'category': ['Introductions', 'Tests']
-        }
-        wp.call(NewPost(post))
+        # def test_post_page(self):
+        #     post = WordPressPost()
+        #     post.title = 'My new title'
+        #     post.content = 'This is the body of my new post.'
+        #     post.terms_names = {
+        #         'post_tag': ['test', 'firstpost'],
+        #         'category': ['Introductions', 'Tests']
+        #     }
+        #     self.wp.call(NewPost(post))
