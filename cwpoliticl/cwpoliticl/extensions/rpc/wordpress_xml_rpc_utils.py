f2a5b990dbb88623e1042b093defda58d09067cc
trujunzhang <trujunzhang@gmail.com>
2016-07-13 14:39:39 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/rpc/wordpress_xml_rpc_utils.py
+++ cwpoliticl/cwpoliticl/extensions/rpc/wordpress_xml_rpc_utils.py
@@ -23,12 +23,14 @@ class WDXmlRPCUtils(object):
         super(WDXmlRPCUtils, self).__init__()
 
     def post(self, item):
-        # Step 1: download the featured image to the template folder.
+        # step 1: download the featured image to the template folder.
         image_location = ImagesDownload.write_cache(item['image'])
+
         # step 2: post to the wordpress via xml_rpc.
         attachment_id = self._post_image_to_wordpress(image_location)
         addpost = self._post_newpost_to_wordpress(item, attachment_id)
-        # Step 3: remove the featured image on the template folder.
+
+        # step 3: remove the featured image on the template folder.
         ImagesDownload.remove_image_location(image_location)
 
         return addpost
