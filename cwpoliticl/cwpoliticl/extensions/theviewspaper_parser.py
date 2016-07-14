8f5ce556a06ec10c93415d6f0300876b0185d48f
trujunzhang <trujunzhang@gmail.com>
2016-07-14 19:56:06 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/theviewspaper_parser.py
+++ cwpoliticl/cwpoliticl/extensions/theviewspaper_parser.py
@@ -8,15 +8,23 @@ class TheViewsPaperParser(BaseParser):
         super(TheViewsPaperParser, self).__init__()
 
     def parse_paginate(self, url, hxs, cache_db, history_db):
-        selector = '//*[@class="container"]/*[@class="row"]/*[@class="col-md-8"]/article/a/@href'
+        selector = '//*[@class="container"]/*[@class="row"]/*[@class="col-md-8"]/article'
         links = hxs.xpath(selector).extract()  # Type: List['unicode']
 
+        count = 1
         for href in links:
+            href_selector = '{}[{}]/a/@href'.format(selector, count)
+            thumbnail_selector = '{}[{}]/a/img/@src'.format(selector, count)
+            count += 1
+
+            href = self.get_value_response(hxs, href_selector)
             # If the link already exist on the history database, ignore it.
             if history_db.check_history_exist(href):
                 continue
 
-            cache_db.save_cache(CacheItem.get_default(url=href, url_from=self.url_from))
+            thumbnail_src = self.get_value_response(hxs, thumbnail_selector)
+
+            cache_db.save_cache(CacheItem.get_default(url=href, thumbnail_url=thumbnail_src, url_from=self.url_from))
 
     def parse(self, url, hxs, wd_rpc, thumbnail_url, access_denied_cookie):
         title = self.get_value_response(hxs, '//*[@class="entry-content"]/*[@class="entry-header"]/h2/a/text()')
