e1c544022c0f5bcda9ff572518ce893f773d52c9
trujunzhang <trujunzhang@gmail.com>
2016-07-14 10:41:22 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/spiders/indianexpress_debug_spider.py
+++ cwpoliticl/cwpoliticl/spiders/indianexpress_debug_spider.py
@@ -8,12 +8,12 @@ class IndianExpressDebugSpider(scrapy.Spider):
     allowed_domains = ["www.indianexpress.com"]
     start_urls = [
         # Pagination
-        # 'http://indianexpress.com/opinion/',
+        'http://indianexpress.com/opinion/',
         # Detail
         # no tags
         # 'http://indianexpress.com/article/opinion/columns/gulberg-society-massacre-ehsan-jafri-zakia-jafri-case-naroda-patiya-conspiracy-under-the-carpet-2877863/'
         # one tag
-        'http://indianexpress.com/article/opinion/columns/prakash-javadekar-hrd-ministry-narendra-modi-cabinet-reshuffle-expansion-2900114/'
+        # 'http://indianexpress.com/article/opinion/columns/prakash-javadekar-hrd-ministry-narendra-modi-cabinet-reshuffle-expansion-2900114/'
     ]
 
     # 'Ignoring response <403 http://www.dnaindia.com/analysis>: HTTP status code is not handled or not allowed'
@@ -52,5 +52,5 @@ class IndianExpressDebugSpider(scrapy.Spider):
                                                                  )
 
     def parse(self, response):
-        # self._indian_express_Parse.parse_paginate(response.url, response, self._cache_db, self._history_db)
-        item = self._indian_express_Parse.parse(response.url, response, self.wd_rpc)
+        self._indian_express_Parse.parse_paginate(response.url, response, self._cache_db, self._history_db)
+        # item = self._indian_express_Parse.parse(response.url, response, self.wd_rpc)
