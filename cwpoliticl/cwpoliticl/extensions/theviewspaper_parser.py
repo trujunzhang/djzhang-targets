2a6cef5d08fb76ceec6b8b3c568d1f34e387bf64
trujunzhang <trujunzhang@gmail.com>
2016-07-14 10:58:46 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/theviewspaper_parser.py
+++ cwpoliticl/cwpoliticl/extensions/theviewspaper_parser.py
@@ -22,10 +22,10 @@ class TheViewsPaperParser(BaseParser):
             cache_db.save_cache(CacheItem.get_default(url=href, url_from=self._url_from))
 
     def parse(self, url, hxs, wd_rpc):
-        title = self.get_value_response(hxs, '//*[@class="entry-content"]/*[@header="entry-header"]/h2/a/text()')
+        title = self.get_value_response(hxs, '//*[@class="entry-content"]/*[@class="entry-header"]/h2/a/text()')
         image = self.get_value_response(hxs, '//*[@class="entry-content"]/*[@class="content"]/p[1]/img/@src')
-        content = self.get_all_value_response(hxs, '//*[@itemprop="articleBody"]/p', max_len=2, sperator='\n' + '\n',
-                                              start_index=2)
+        content = self.get_all_value_response(hxs, '//*[@class="entry-content"]/*[@class="content"]/p',
+                                              max_len=2, sperator='\n' + '\n', start_index=2)
 
         tags = hxs.xpath(
             '//*[@class="entry-content"]/*[@class="post-meta"]/*[@class="categories-links"]/a/text()').extract()
