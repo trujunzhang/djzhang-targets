07db0d71e9fbccaaa6a535ff3c6b9880bdc5b551
trujunzhang <trujunzhang@gmail.com>
2016-07-15 14:49:53 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/dailyo_parser.py
+++ cwpoliticl/cwpoliticl/extensions/dailyo_parser.py
@@ -36,13 +36,15 @@ class DailyoParser(BaseParser):
             cache_db.save_cache(CacheItem.get_default(url=href, thumbnail_url=thumbnail_src, url_from=self.url_from))
 
     def parse(self, url, hxs, wd_rpc, thumbnail_url, access_denied_cookie):
-        title = self.get_value_response(hxs, '//*[@class="entry-content"]/*[@class="entry-header"]/h2/a/text()')
-        image_src = self._get_image(hxs, '//*[@class="entry-content"]/*[@class="content"]/p[1]/img/@srcset')
-        content = self.get_all_value_response(hxs, '//*[@class="entry-content"]/*[@class="content"]/p',
+        title = self.get_value_response(hxs, '//*[@id="header-story"]/*[@class="header-inner"]/h1/text()')
+        image_src = self._get_image_src(hxs, '//*[@id="header-story"]/@style')
+
+        content = self.get_all_value_response(hxs,
+                                              '//*[@itemprop="articleBody"]/*[@class="page-content"]/div/div/*[@id="middle_container"]/*[@class="story-middle"]/*[@class="mediumcontent"]',
                                               max_len=2, sperator='\n' + '\n', start_index=2)
 
         tags = hxs.xpath(
-            '//*[@class="entry-content"]/*[@class="post-meta"]/*[@class="categories-links"]/a/text()').extract()
+            '//*[@class="bottom-full"]/*[@class="bottom_cont_tg"]/*[@class="story-middle"]/*[@id="taglist"]/a/text()').extract()
 
         item = WDPost.get_default(url, self.url_from, title, image_src, thumbnail_url, content, tags,
                                   access_denied_cookie=access_denied_cookie)
@@ -51,13 +53,11 @@ class DailyoParser(BaseParser):
 
         return item
 
-    def _get_image(self, hxs, selector):
-        image = self.get_value_response(hxs, selector)
-        srcset = image.split(',')
-        if len(srcset) > 0:
-            src_format = srcset[len(srcset) - 1].strip()
-            src_split = src_format.split(' ')
-            if len(src_split) == 2:
-                return src_split[0]
+    def _get_image_src(self, hxs, selector):
+        image_style = self.get_value_response(hxs, selector)
 
-        return ""
+        split = image_style.split('"')
+        if len(split) >= 2:
+            return split[1]
+
+        return ''
