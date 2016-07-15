3a98c577dd40435dd3582e82b5de7b656340c2ce
trujunzhang <trujunzhang@gmail.com>
2016-07-15 20:30:43 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/deccanchronicle_parser.py
+++ cwpoliticl/cwpoliticl/extensions/deccanchronicle_parser.py
@@ -34,13 +34,12 @@ class DeccanchronicleParser(BaseParser):
     def parse(self, url, hxs, wd_rpc, thumbnail_url, access_denied_cookie):
         title = self.get_value_response(hxs, '//*[@class="story-main"]/h1/span/text()')
         image_src = self.get_value_response(hxs,
-                                            '//*[@class="story-main"]/*[@id="story-body"]/*[@class="cover"]/img/@src')
+                                            '//*[@class="story-main"]/*[@class="story-body"]/*[@class="cover"]/img/@src')
 
         content = self.get_all_value_response(hxs,
-                                              '//*[@class="story-main"]/*[@id="story-body"]/*[@id="storyBody"]/p/text()')
+                                              '//*[@class="story-main"]/*[@class="story-body"]/*[@id="storyBody"]/p/text()')
 
-        tags = hxs.xpath(
-            '//*[@class="story-main"]/*[@id="story-body"]/*[@class="articleTags"]/a/text()').extract()
+        tags = hxs.xpath('//*[@class="story-main"]/*[@class="story-body"]/*[@class="articleTags"]/a/text()').extract()
 
         item = WDPost.get_default(url, self.url_from, title, image_src, thumbnail_url, content, tags,
                                   access_denied_cookie=access_denied_cookie)
