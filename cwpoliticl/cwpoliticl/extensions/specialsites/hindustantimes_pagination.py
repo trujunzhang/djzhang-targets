c2aecf62d83bd41b9d4300d1ce737b89cd0b3ba9
trujunzhang <trujunzhang@gmail.com>
2016-07-16 10:38:54 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/specialsites/hindustantimes_pagination.py
+++ cwpoliticl/cwpoliticl/extensions/specialsites/hindustantimes_pagination.py
@@ -10,22 +10,35 @@ class HindustantimesPaginationScraper(object):
     def parse_pagination(self, url, hxs, cache_db, history_db):
 
         # Top picks
+        # |              |              |
+        # | single story | 3 items list |
+        # |              |              |
+        self._parse_row_container(url, hxs, cache_db, history_db,
+                                  '//*[@id="div_storyContent"]/*[@class="row_container"][2]')
+
         # self._parse_single_photo_block_for_pagination(url, hxs, cache_db, history_db,
         #                                               '//*[@class="top_single_story_photo"]')
         # self._parse_block_for_pagination(url, hxs, cache_db, history_db, '//*[@class="hm_topstory_3_story"]/ul/li')
+        #
+        # # columns
+        # row_container = '//*[@class="row_container"]/*[@class="col_2 india_headlines"]'
+        # lists = hxs.xpath(row_container)
+        # for idx, link in enumerate(lists):
+        #     single_photo_section = '//*[@class="row_container"]/*[@class="col_2 col_2_right_margin india_topNews"]'.format(
+        #         row_container, (idx + 1))
+        #     # lists_section = '//*[@class="row_container"]/*[@class="col_2 india_headlines"]'.format(
+        #     #     row_container, (idx + 1))
+        #
+        #     self._parse_single_photo_block_for_pagination(url, hxs, cache_db, history_db, single_photo_section)
 
-        # columns
-        row_container = '//*[@class="row_container"]/*[@class="col_2 india_headlines"]'
-        lists = hxs.xpath(row_container)
-        for idx, link in enumerate(lists):
-            single_photo_section = '//*[@class="row_container"]/*[@class="col_2 col_2_right_margin india_topNews"]'.format(
-                row_container, (idx + 1))
-            # lists_section = '//*[@class="row_container"]/*[@class="col_2 india_headlines"]'.format(
-            #     row_container, (idx + 1))
+        # self._parse_block_for_pagination(url, hxs, cache_db, history_db, '//*[@class="hm_topstory_3_story"]/ul/li')
 
-            self._parse_single_photo_block_for_pagination(url, hxs, cache_db, history_db, single_photo_section)
+    def _parse_row_container(self, url, hxs, cache_db, history_db, row_container_selector):
+        single_selector = '{}/div/div/div[1]/div[1]'.format(row_container_selector)
+        self._parse_single_photo_block_for_pagination(url, hxs, cache_db, history_db, single_selector)
 
-            # self._parse_block_for_pagination(url, hxs, cache_db, history_db, '//*[@class="hm_topstory_3_story"]/ul/li')
+        items_list_selector = '{}/div/div/div[2]/ul/li'.format(row_container_selector)
+        self._parse_block_for_pagination(url, hxs, cache_db, history_db, items_list_selector)
 
     def _parse_single_photo_block_for_pagination(self, url, hxs, cache_db, history_db, select_block):
         lists = hxs.xpath(select_block)
