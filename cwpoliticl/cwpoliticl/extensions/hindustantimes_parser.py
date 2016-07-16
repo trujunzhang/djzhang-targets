69ab9a8310e0f02ea81b19089bcbe5c2c95ec240
trujunzhang <trujunzhang@gmail.com>
2016-07-16 16:54:52 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/hindustantimes_parser.py
+++ cwpoliticl/cwpoliticl/extensions/hindustantimes_parser.py
@@ -3,6 +3,13 @@ from cwpoliticl.items import WDPost
 
 
 class HindustantimesParser(BaseParser):
+    page_selector_dict = {
+        "title": '//*[@class="story_pg_head"]/h1/text()',
+        "image": '//*[@class="story_top_news"]/*[@class="news_photo"]/img/@src',
+        "content": '//*[@id="div_storyContent"]/p/text()',
+        "tags": '//*[@class="story_tags"]/li/a/text()',
+    }
+
     def __init__(self):
         from cwpoliticl.scraped_websites import WebsiteTypes
         self.url_from = WebsiteTypes.hindustantimes.value
@@ -23,12 +30,10 @@ class HindustantimesParser(BaseParser):
         HindustantimesPaginationScraper(self, self.url_from, url, hxs, cache_db, history_db).parse_pagination()
 
     def parse(self, url, hxs, wd_rpc, thumbnail_url, access_denied_cookie):
-        title = self.get_value_response(hxs, '//*[@class="story_pg_head"]/h1/text()')
-        image_src = self.get_value_with_urljoin(hxs,
-                                                '//*[@class="story_top_news"]/*[@class="news_photo"]/img/@src',
-                                                url)
-        content = self.get_all_value_response(hxs, '//*[@id="div_storyContent"]/p/text()')
-        tags = hxs.xpath('//*[@class="story_tags"]/li/a/text()').extract()
+        title = self.get_value_response(hxs, self.page_selector_dict['title'])
+        image_src = self.get_value_with_urljoin(hxs, self.page_selector_dict['image'], url)
+        content = self.get_all_value_response(hxs, self.page_selector_dict['content'])
+        tags = hxs.xpath(self.page_selector_dict['tags']).extract()
 
         item = WDPost.get_default(url, self.url_from, title, image_src, thumbnail_url, content, tags,
                                   access_denied_cookie=access_denied_cookie)
