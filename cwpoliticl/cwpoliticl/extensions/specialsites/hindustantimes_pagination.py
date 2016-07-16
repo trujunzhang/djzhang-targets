5fbaacac146b9c1d0fb39129de1374f7b3d10135
trujunzhang <trujunzhang@gmail.com>
2016-07-16 15:28:54 +0800

    cwpoliticl.

--- cwpoliticl/cwpoliticl/extensions/specialsites/hindustantimes_pagination.py
+++ cwpoliticl/cwpoliticl/extensions/specialsites/hindustantimes_pagination.py
@@ -63,7 +63,7 @@ class HindustantimesPaginationScraper(object):
 
     def _right_panel_parse(self, url, hxs, cache_db, history_db):
         # top news
-        self._ajax_for_block(url, hxs, cache_db, history_db, dict)
+        self._ajax_for_block(url, hxs, cache_db, history_db, self.right_panel_selector_dict)
         pass
 
     def _left_panel_parse(self, url, hxs, cache_db, history_db):
@@ -108,6 +108,6 @@ class HindustantimesPaginationScraper(object):
             self._parse_single_photo_block_for_pagination(url, hxs, cache_db, history_db, dict, list_select_block)
 
     def _ajax_for_block(self, url, hxs, cache_db, history_db, dict):
-        ajax_url = self.parser.get_url_joinI(url, dict['ajax'])
+        ajax_url = self.parser.get_url_join(url, dict['ajax'])
         response = self.parser.ajax(ajax_url)
         x = 0
