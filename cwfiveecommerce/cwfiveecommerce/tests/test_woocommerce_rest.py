163008955c062f92d1fa9a59200177d7ae67ff39
trujunzhang <trujunzhang@gmail.com>
2016-07-21 22:20:28 +0800

    cwfiveecommerce.

--- cwfiveecommerce/cwfiveecommerce/tests/test_woocommerce_rest.py
+++ cwfiveecommerce/cwfiveecommerce/tests/test_woocommerce_rest.py
@@ -28,19 +28,19 @@ class WoocommerceRestTest(unittest.TestCase):
             "regular_price": "21.99",
             "description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
             "short_description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-            "images": [
-                {
-                    "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg",
-                    "position": 0
-                },
-                {
-                    "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg",
-                    "position": 1
-                }
-            ]
+            # "images": [
+            #     {
+            #         "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg",
+            #         "position": 0
+            #     },
+            #     {
+            #         "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg",
+            #         "position": 1
+            #     }
+            # ]
         }
 
-        # r = self.wcapi.get("")
+        # r = self.wcapi.get("products")
         r = self.wcapi.post("products", data)
         code = r.status_code
         if code == 201:
