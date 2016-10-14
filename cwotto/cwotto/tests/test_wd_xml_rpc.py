d2ea43e81a9453583136b0e8aa6fdb757d0085d5
trujunzhang <trujunzhang@gmail.com>
2016-10-14 22:02:29 +0800

    product.csv

--- cwotto/cwotto/tests/test_wd_xml_rpc.py
+++ cwotto/cwotto/tests/test_wd_xml_rpc.py
@@ -26,10 +26,12 @@ class WDXmlRpcTest(unittest.TestCase):
         widget.title = 'Widget'
         widget.content = 'AJC Kurzblazer bedruckt oder uni'
         widget.custom_fields = []
-        widget.custom_fields.append({
-            'key': 'price',
-            'value': 2
-        })
+        widget.custom_fields.append(
+            [
+                {'key': '_regular_price', 'value': 20},
+                {'key': '_sale_price', 'value': 14}
+            ]
+        )
         widget.id = self.client.call(posts.NewPost(widget))
         self.assertNotEqual(widget.id, None)
 
