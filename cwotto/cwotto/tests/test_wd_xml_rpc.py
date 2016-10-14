ba3dcdb220d8ff27192a7b93b538bb396b43dab0
trujunzhang <trujunzhang@gmail.com>
2016-10-14 21:42:43 +0800

    product.csv

--- cwotto/cwotto/tests/test_wd_xml_rpc.py
+++ cwotto/cwotto/tests/test_wd_xml_rpc.py
@@ -16,6 +16,9 @@ class WDXmlRpcTest(unittest.TestCase):
         # self.image_link = 'http://theviewspaper.net/wp-content/uploads/WordsOfTerror-1024x576.jpg'
         # self.image_link = 'http://localhost:8888/politicl/wp-content/uploads/2016/07/picture-324x160.jpeg'
 
+    def import_product_to_wd(self, product):
+        pass
+
     def test_multiple_posts(self):
         from wordpress_xmlrpc.methods import posts
         posts = self.client.call(posts.GetPosts())
@@ -24,4 +27,6 @@ class WDXmlRpcTest(unittest.TestCase):
         path = '{}/{}'.format(current_paths, 'utils/items_otto.json')
         with open(path) as data_file:
             data = json.load(data_file)
-        x = 0
+
+        product = data[0]
+        self.import_product_to_wd(product)
