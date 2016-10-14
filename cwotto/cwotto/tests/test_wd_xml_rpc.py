dcd945bc3a2359b43a2b97da496494110dd3b51d
trujunzhang <trujunzhang@gmail.com>
2016-10-14 21:07:08 +0800

    product.csv

--- cwotto/cwotto/tests/test_wd_xml_rpc.py
+++ cwotto/cwotto/tests/test_wd_xml_rpc.py
@@ -7,11 +7,12 @@ from cwotto import settings
 class WDXmlRpcTest(unittest.TestCase):
     def setUp(self):
         from wordpress_xmlrpc import Client
-        host = "{}/{}".format(settings.WD_HOST, settings.WD_COLLECTION)
+        host = "http://{}/{}".format(settings.WD_HOST, settings.WD_COLLECTION)
         self.client = Client(host, settings.WD_USER, settings.WD_PASSWD)
 
+        x = 0
         # self.image_link = 'http://theviewspaper.net/wp-content/uploads/WordsOfTerror-1024x576.jpg'
-        self.image_link = 'http://localhost:8888/politicl/wp-content/uploads/2016/07/picture-324x160.jpeg'
+        # self.image_link = 'http://localhost:8888/politicl/wp-content/uploads/2016/07/picture-324x160.jpeg'
 
     def test_multiple_posts(self):
         from wordpress_xmlrpc.methods import posts
