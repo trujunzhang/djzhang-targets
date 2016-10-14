40c1f5a5d1e653b7bb04eddb89c03c3fc5c1297b
trujunzhang <trujunzhang@gmail.com>
2016-10-14 22:34:18 +0800

    product.csv

---
+++ cwotto/cwotto/tests/test_woocommerce_rest.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import unittest
+
+from cwotto import settings
+from woocommerce import API
+
+
+class WDWoocommerceRestTest(unittest.TestCase):
+    def setUp(self):
+        self.wcapi = API(
+            url="http://{}".format(settings.WD_HOST),
+            consumer_key=settings.consumer_key,
+            consumer_secret=settings.consumer_secret
+        )
+
+    # def test_posts_list(self):
+    #     from wordpress_xmlrpc.methods import posts
+    #     posts = self.client.call(posts.GetPosts())
+
+    def import_product_to_wd(self, product):
+        self.assertNotEqual(product.id, None)
+
+    def test_multiple_posts(self):
+        current_paths = os.path.dirname(__file__).replace('/tests', '')
+        path = '{}/{}'.format(current_paths, 'utils/items_otto.json')
+        with open(path) as data_file:
+            data = json.load(data_file)
+
+        product = data[0]
+        self.import_product_to_wd(product)
