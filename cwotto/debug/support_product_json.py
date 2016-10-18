c1efc5dcde496970842daf906395a7f8c51e7fcf
trujunzhang <trujunzhang@gmail.com>
2016-10-18 23:37:51 +0800

    otto

--- cwotto/debug/support_product_json.py
+++ cwotto/debug/support_product_json.py
@@ -7,13 +7,13 @@ class ProductJsonSupport(object):
         super(ProductJsonSupport, self).__init__()
 
     def get_variation_json(self):
-        data = None
+        data = []
         variations__json = "Apple_iPhone_SE_4/variations/535544121.json"
         json_file = "{}/{}".format(self.debug_folder, variations__json)
         with open(json_file) as data_file:
             data = json.load(data_file)
 
-        return data
+        return data[0]
 
     def get_product_json(self):
         data = None
