5238c6011560abe041cf58c2976e0c85d717c855
trujunzhang <trujunzhang@gmail.com>
2016-10-18 16:44:40 +0800

    otto

---
+++ cwotto/cwotto/parser/products/otto_variations_util.py
@@ -0,0 +1,7 @@
+class OttoVariationsUtil(object):
+    def __init__(self, product_json):
+        self.product_json = product_json
+        super(OttoVariationsUtil, self).__init__()
+
+    def get_variations_children(self):
+        pass
--- cwotto/cwotto/parser/products/variable_products.py
+++
@@ -1,5 +0,0 @@
-class VariableProductParse(object):
-    def __init__(self):
-        super(VariableProductParse, self).__init__()
-
-
