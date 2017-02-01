14635b630bc3e6cc4346db1a370e0a85a7c3b3cb
trujunzhang <trujunzhang@gmail.com>
2017-02-02 00:32:08 +0800

    responsive html.

--- cwresponsivesites/cwresponsivesites/tests/test_pynliner.py
+++ cwresponsivesites/cwresponsivesites/tests/test_pynliner.py
@@ -2,9 +2,10 @@
 
 import unittest
 
+# Todo: Have uninstalled Pynliner.
 
 class pynlinerTest(unittest.TestCase):
     def test_convert_to_inline_css(self):
-
-
+        url = 'http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'
+        # filename = wget.download(url)
         x = 0
